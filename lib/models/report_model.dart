c7577eccde6dbd998637a233242c8187d6cd0d16
Siyanfr <carosuscean@yahoo.com>
2026-09-18 16:57:40 +0800

    Added empty files with respective names to start off other screens specifically for the citizens dashboard screen. also added widget empty files to strengthen the structure of the application

---
+++ lib/models/report_model.dart
@@ -0,0 +1,23 @@
+class Report {
+  final String id;
+  final String reporterId;
+  final String? photoUrl;
+  final String description;
+  final double? latitude;
+  final double? longitude;
+  final String locationName;
+  final DateTime timestamp;
+  final String status; // 'Pending', 'Resolved', or 'Rejected'
+
+  Report({
+    required this.id,
+    required this.reporterId,
+    this.photoUrl,
+    required this.description,
+    this.latitude,
+    this.longitude,
+    required this.locationName,
+    required this.timestamp,
+    required this.status,
+  });
+}
